509ef95366998ba1487ab775299f73b3dde05f0a
Dimas Abreu Dutra <dimasadutra@gmail.com>
2014-11-06 14:45:01 -0200

    Created basic MainWindow layout.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -4,6 +4,7 @@
 #include <QAction>
 #include <QDialogButtonBox>
 #include <QFormLayout>
+#include <QGroupBox>
 #include <QLabel>
 #include <QSerialPortInfo>
 #include <QToolBar>
@@ -123,28 +124,129 @@ MainWindow::MainWindow(QWidget *parent) :
     mainToolBar->setMovable(false);
     mainToolBar->addAction(showSettings);
     
-    auto *angularGauge = new AngularSvgGauge(":/images/angular-gauge.svg");
-    angularGauge->setValueRange(0, 200);
-    angularGauge->setAngleRange(-90, 90);
-    angularGauge->setNumMajorTicks(5);
-    angularGauge->setValueLabelPos(120, 220);
-    angularGauge->setTextColor(QColor("white"));
-    angularGauge->setValue(30);
-    //angularGauge->addLabel(u8"Oil temperature (\u00B0F)", 120, 200);
-    //m_updater.link("oil temperature", angularGauge);
-
-    auto *cht1Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
+    auto rpmGauge = new AngularSvgGauge(":/images/angular-gauge.svg");
+    rpmGauge->setValueRange(0, 200);
+    rpmGauge->setAngleRange(-90, 90);
+    rpmGauge->setNumMajorTicks(5);
+    rpmGauge->setValueLabelPos(120, 220);
+    rpmGauge->setTextColor(QColor("white"));
+    //TODO: link gauges
+    
+    auto cht1Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
     cht1Gauge->setValueRange(0, 500);
     cht1Gauge->setNumMajorTicks(3);
-    cht1Gauge->setValue(250);
-    //m_updater.link("cht1", cht1Gauge);
+
+    auto cht2Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
+    cht2Gauge->setValueRange(0, 500);
+    cht2Gauge->setNumMajorTicks(3);
+
+    auto cht3Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
+    cht3Gauge->setValueRange(0, 500);
+    cht3Gauge->setNumMajorTicks(3);
+
+    auto cht4Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
+    cht4Gauge->setValueRange(0, 500);
+    cht4Gauge->setNumMajorTicks(3);
     
-    auto *chtLayout = new QVBoxLayout;
+    auto chtLayout = new QVBoxLayout;
+    chtLayout->setSpacing(0);
     chtLayout->addWidget(cht1Gauge);
+    chtLayout->addWidget(cht2Gauge);
+    chtLayout->addWidget(cht3Gauge);
+    chtLayout->addWidget(cht4Gauge);
     
-    auto *mainLayout = new QHBoxLayout;
-    mainLayout->addWidget(angularGauge);
-    mainLayout->addItem(chtLayout);
+    auto chtGroupBox = new QGroupBox("CHT");
+    chtGroupBox->setAlignment(Qt::AlignHCenter);
+    chtGroupBox->setLayout(chtLayout);
+
+    auto oilPressGauge = new AngularSvgGauge(":/images/angular-gauge.svg");
+    oilPressGauge->setValueRange(0, 200);
+    oilPressGauge->setAngleRange(-90, 90);
+    oilPressGauge->setNumMajorTicks(5);
+    oilPressGauge->setValueLabelPos(120, 220);
+    oilPressGauge->setTextColor(QColor("white"));
+    
+    auto column1Layout = new QVBoxLayout;
+    column1Layout->addWidget(rpmGauge);
+    column1Layout->addWidget(chtGroupBox);
+    column1Layout->addWidget(oilPressGauge);
+ 
+    auto mapGauge = new AngularSvgGauge(":/images/angular-gauge.svg");
+    mapGauge->setValueRange(0, 200);
+    mapGauge->setAngleRange(-90, 90);
+    mapGauge->setNumMajorTicks(5);
+    mapGauge->setValueLabelPos(120, 220);
+    mapGauge->setTextColor(QColor("white"));
+    
+    auto egt1Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
+    egt1Gauge->setValueRange(0, 500);
+    egt1Gauge->setNumMajorTicks(3);
+
+    auto egt2Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
+    egt2Gauge->setValueRange(0, 500);
+    egt2Gauge->setNumMajorTicks(3);
+
+    auto egt3Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
+    egt3Gauge->setValueRange(0, 500);
+    egt3Gauge->setNumMajorTicks(3);
+
+    auto egt4Gauge = new LinearSvgGauge(":/images/horizontal-gauge.svg");
+    egt4Gauge->setValueRange(0, 500);
+    egt4Gauge->setNumMajorTicks(3);
+    
+    auto egtLayout = new QVBoxLayout;
+    egtLayout->setSpacing(0);
+    egtLayout->addWidget(egt1Gauge);
+    egtLayout->addWidget(egt2Gauge);
+    egtLayout->addWidget(egt3Gauge);
+    egtLayout->addWidget(egt4Gauge);
+    
+    auto egtGroupBox = new QGroupBox("EGT");
+    egtGroupBox->setAlignment(Qt::AlignHCenter);
+    egtGroupBox->setLayout(egtLayout);
+
+    auto oilTempGauge = new AngularSvgGauge(":/images/angular-gauge.svg");
+    oilTempGauge->setValueRange(0, 200);
+    oilTempGauge->setAngleRange(-90, 90);
+    oilTempGauge->setNumMajorTicks(5);
+    oilTempGauge->setValueLabelPos(120, 220);
+    oilTempGauge->setTextColor(QColor("white"));
+    
+    auto column2Layout = new QVBoxLayout;
+    column2Layout->addWidget(mapGauge);
+    column2Layout->addWidget(egtGroupBox);
+    column2Layout->addWidget(oilTempGauge);
+
+    auto fuelPressGauge = new AngularSvgGauge(":/images/angular-gauge.svg");
+    fuelPressGauge->setValueRange(0, 200);
+    fuelPressGauge->setAngleRange(-90, 90);
+    fuelPressGauge->setNumMajorTicks(5);
+    fuelPressGauge->setValueLabelPos(120, 220);
+    fuelPressGauge->setTextColor(QColor("white"));
+
+    auto fuelLevelAGauge = new AngularSvgGauge(":/images/angular-gauge.svg");
+    fuelLevelAGauge->setValueRange(0, 200);
+    fuelLevelAGauge->setAngleRange(-90, 90);
+    fuelLevelAGauge->setNumMajorTicks(5);
+    fuelLevelAGauge->setValueLabelPos(120, 220);
+    fuelLevelAGauge->setTextColor(QColor("white"));
+
+    auto fuelLevelBGauge = new AngularSvgGauge(":/images/angular-gauge.svg");
+    fuelLevelBGauge->setValueRange(0, 200);
+    fuelLevelBGauge->setAngleRange(-90, 90);
+    fuelLevelBGauge->setNumMajorTicks(5);
+    fuelLevelBGauge->setValueLabelPos(120, 220);
+    fuelLevelBGauge->setTextColor(QColor("white"));
+    
+    auto column3Layout = new QVBoxLayout;
+    column3Layout->addWidget(fuelPressGauge);
+    column3Layout->addWidget(fuelLevelAGauge);
+    column3Layout->addWidget(fuelLevelBGauge);
+    
+    auto mainLayout = new QHBoxLayout;
+    mainLayout->addItem(column1Layout);
+    mainLayout->addItem(column2Layout);
+    mainLayout->addItem(column3Layout);
     
     QWidget *centralWidget = new QWidget;
     centralWidget->setLayout(mainLayout);
